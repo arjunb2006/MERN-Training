0eb99c4f74297d5f26a9144189c824a609fa5ee7
Arjun <arjunbraj2006@gmail.com>
2026-09-18 11:42:04 +0530

    MERN_TRAINING

---
+++ react-demo/src/App.jsx
@@ -0,0 +1,44 @@
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello Firends</h1>
+//       <p>Chai Peelo</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+import "./App.css";
+function App(){
+  const tarinerName="Nirmal";
+  const trainingDay=3;
+  return(
+<main className="app">
+<section className="welcome-card">
+<p className="day-label">React Day {trainingDay}</p>
+<h1 className="title">College Course Explorer </h1>
+<p>Trainer:{tarinerName}</p>
+      <p>React Learning Project</p>
+      <p>Used React,JSX,componets,CSS</p>
+      </section>   
+    </main>       
+  );
+}
+export default App;
+
+
+import "./App.css"; 
+import Navbar from "./assets/navbar";
+function App() {
+  return (
+    <div>
+      <main id="home">
+        <h1>campus <Connecct</h1>
+        <P>Welcome to college Events and Clubs</P>  
+        </main>
+        </div>
+  );
+}
+  export default App;
+
+  
---
+++ react-demo/src/assets/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero(){
+    return(
+        <section className="hero">
+            <p className="hero-label">College Events and Clubs</p>
+            <h1>Discover what is Happening</h1>
+            <p className ="hero-description">
+                Find workshop,sports activities,club meeting
+            </p>
+            <a className ="hero-button" href="#events">Explore Events</a>
+
+        </section>
+    );
+}
+export default Hero;
